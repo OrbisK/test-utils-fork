8c004422db2a48b67a4bb7caa26dcd8bee88fcae
OrbisK <robin.kehl@singular-it.de>
2025-10-14 12:21:17 +0200

    chore: add reproduction

--- examples/app-vitest-browser/nuxt.config.ts
+++ examples/app-vitest-browser/nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     rootId: 'nuxt-test',
   },
+
   compatibilityDate: '2024-04-03',
+  modules: ['@nuxtjs/i18n'],
+    i18n: {},
 })
