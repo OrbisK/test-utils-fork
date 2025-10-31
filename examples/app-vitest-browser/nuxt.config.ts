f4ad82a3ef2ac6e26337db5a77f6d2eb7c111953
OrbisK <robin.kehl@singular-it.de>
2025-10-31 15:24:41 +0100

    reproduction: sentry

--- examples/app-vitest-browser/nuxt.config.ts
+++ examples/app-vitest-browser/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@sentry/nuxt/module'],
   devtools: { enabled: true },
   app: {
     rootId: 'nuxt-test',
---
+++ examples/app-vitest-browser/sentry.client.config.ts
@@ -0,0 +1,6 @@
+import * as Sentry from '@sentry/nuxt'
+
+Sentry.init({
+  // If set up, you can use your runtime config here
+  enabled: false,
+})
